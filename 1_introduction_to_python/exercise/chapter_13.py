f530a8968e0a3618cfab0b4123011c5debd6f736
chrysan19theme <tsubasaerfan@gmail.com>
2025-08-20 08:39:45 +0900

    chapter_13

---
+++ 1_introduction_to_python/exercise/chapter_13.py
@@ -0,0 +1,9 @@
+from datetime import date
+from datetime import time
+
+#13.2
+today_string=date.today()
+print(today_string)
+
+#13.3
+print(today_string.date)
--- 1_introduction_to_python/sample.py
+++ 1_introduction_to_python/sample.py
@@ -1,15 +1,4 @@
-import re
-import string
+import calendar
+from datetime import date
+from datetime import timedelta
 
-source='I wish I may, I wish I might Have a dish of fish tonight.'
-
-print(re.findall(r'wish',source))
-print(re.findall(r'wish|fish',source))
-print(re.findall(r'^wish',source))
-print(re.findall(r'^I wish',source))
-print(re.findall(r'tonight\.$',source))
-print(re.findall(r'[wf]ish',source))
-print(re.findall(r'[wsh]+',source))
-print(re.findall(r'ght\W',source))
-print(re.findall(r'I (?=wish)',source))
-print(re.findall(r'(?<=I) wish',source))
