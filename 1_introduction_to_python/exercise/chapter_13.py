929f13d3c2a174c42a2bf45ec6e4b2408717aace
chrysan19theme <ts.kikuchi@iftc.co.jp>
2025-08-20 08:58:40 +0900

    chapter_13

--- 1_introduction_to_python/exercise/chapter_13.py
+++ 1_introduction_to_python/exercise/chapter_13.py
@@ -1,9 +1,23 @@
 from datetime import date
 from datetime import time
-
+import time as tm
+from datetime import timedelta
 #13.2
-today_string=date.today()
+today_string='2025/08/20'
 print(today_string)
 
 #13.3
-print(today_string.date)
+fmt='%Y/%m/%d'
+fmt2='%d'
+today=tm.strptime(today_string,fmt)
+print(tm.strftime(fmt2,today))
+
+#13.4
+my_birth=date(1998,4,3)
+
+#13.5
+print(my_birth.strftime('%A'))
+
+#13.6
+target_day=my_birth+timedelta(days=10000)
+print(target_day)
